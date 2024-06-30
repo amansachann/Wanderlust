b9add4997d99c129b2e0cd0b5230b4074fe96791
Aman Sachan <codewithaman78@gmail.com>
2024-06-30 14:58:58 -0700

    New and Create Route

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const Listing = require("./models/listing.js");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main()
@@ -38,18 +38,53 @@ app.get("/listings", (req, res) => {
 });
 
 // ----------------------------
+// New Route
+// ----------------------------
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+// ----------------------------
 // Show Route
 // ----------------------------
 app.get("/listings/:id", (req, res) => {
   const { id } = req.params;
   Listing.findById(id)
     .then((listing) => {
-      res.render("listings/show.ejs", {listing});
+      res.render("listings/show.ejs", { listing });
     })
     .catch((err) => {
       console.log(err);
     });
 });
+
+// ------------------------------
+// Create Route - Aman Version
+// ------------------------------
+app.post("/listings", (req, res) => {
+  const listing = req.body.listing;
+  const newListing = new Listing(listing);
+  newListing
+    .save()
+    .then((result) => {
+      console.log("New Listing Added");
+      console.log(result);
+      res.redirect("/listings");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// -------------------------------
+// Create Route - Sraddha Version
+// -------------------------------
+// app.post("/listings", async (req, res) => {
+//   const newListing = new Listing(req.body.listing);
+//   await newListing.save();
+//   res.redirect("/listings");
+// });
+
 // ----------------------------
 // Just for Testing Purpose
 // ----------------------------
