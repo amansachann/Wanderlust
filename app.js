721df54b20fed13a4456258f864c59fc2ad7daa6
Aman Sachan <codewithaman78@gmail.com>
2024-06-30 16:24:18 -0700

    Creating Boilerplate using ejs-mate

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const port = 8080;
 const path = require("path");
 const Listing = require("./models/listing.js");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main()
