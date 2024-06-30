d1ac8c5cc188800948a7f83d46691382dddc7590
Aman Sachan <codewithaman78@gmail.com>
2024-06-30 08:05:05 -0700

    Listing Model

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const port = 8080;
+const path = require("path");
+const Listing = require("./models/listing.js");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main()
@@ -17,6 +23,22 @@ async function main() {
 
 app.get("/", (req, res) => {
   res.send("Hi, I am root...");
+});
+
+app.get("/testListing", (req, res) => {
+  const sampleListing = new Listing({
+    title: "Beautiful Beach House",
+    description: "A lovely beach house with stunning ocean views and modern amenities.",
+    price: 350000,
+    location: "Madgaon, Goa",
+    country: "India",
+  });
+
+  sampleListing.save().then((result)=>{
+    console.log(result);
+    console.log("Sample was saved");
+    res.send("Successful testing"); 
+  }); 
 });
 
 app.listen(port, () => {
---
+++ models/listing.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default: "https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg", 
+    set: (v) =>
+      v === ""
+        ? "https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg"
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
