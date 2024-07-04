7fde6e484f1b053c7a21f42af4832c60abb96d24
Aman Sachan <codewithaman78@gmail.com>
2024-07-03 17:39:18 -0700

    Validation for schema

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -97,8 +98,10 @@ app.get("/listings/:id", wrapAsync(async(req, res)=>{
 app.post(
   "/listings",
   wrapAsync(async (req, res, next) => {
-    if(!req.body.listing) {
-      throw new ExpressError(400, "Send valid data for listing")
+    let result = listingSchema.validate(req.body);
+    console.log(result);
+    if(result.error) {
+      throw new ExpressError(400, result.error);
     }
     const newListing = new Listing(req.body.listing);
     await newListing.save();
@@ -214,7 +217,7 @@ app.all("*", (req, res, next)=>{
 app.use((err, req, res, next) => {
   let {statusCode=500, message="Something went wrong!"} = err;
   // res.status(statusCode).send(message);
-  res.render("error.ejs", {statusCode, message});
+  res.status(statusCode).render("error.ejs", {statusCode, message});
 });
 
 app.listen(port, () => {
